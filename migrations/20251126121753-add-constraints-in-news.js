870b927a1f283f9f76c20dd28c535182307d5ffe
Amar Raul <143498791+MyResearchRoom@users.noreply.github.com>
2025-11-27 16:18:15 +0530

    Update 20251126121753-add-constraints-in-news.js

--- migrations/20251126121753-add-constraints-in-news.js
+++ migrations/20251126121753-add-constraints-in-news.js
@@ -3,11 +3,11 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // await queryInterface.addConstraint("news_views", {
-    //   fields: ["newsId", "visitorId"],
-    //   type: "unique",
-    //   name: "unique_newsId_visitorId",
-    // });
+    await queryInterface.addConstraint("news_views", {
+      fields: ["newsId", "visitorId"],
+      type: "unique",
+      name: "unique_newsId_visitorId",
+    });
   },
 
   async down(queryInterface, Sequelize) {
